ff3f9f0d92c3203282b3c1cd0368773c437198f2
Haden MV <med4en@gmail.com>
2023-08-15 11:18:05 +0000

    few fixes

--- clients/hanaye/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'https://hanaye-9261d70cce3c.herokuapp.com',
-};
--- clients/rcadmin/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'https://hanaye-9261d70cce3c.herokuapp.com',
-  mapApiKey: 'AIzaSyAp-au0nM_esKXU4E89iY1JmaeX43tCpS0',
-};
